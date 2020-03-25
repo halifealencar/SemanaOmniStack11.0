dd46bae0f5470f6685b344d3d8db3825b515cce2
Halife Alencar <halife.alencar@gmail.com>
2020-03-24 20:34:36 -0400

    [TSK] Aprendendo rotas do node [IMP] Instalado depedência nodemon somente para desenvolvimento

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,33 @@ const express = require('express');
 //cria aplicação
 const app = express();
 
-app.get('/', (request, response) =>{
-    return response.json({
-        evento: 'teste',
-        allow: 'anonimo'
-    });
+//tem que vir antes, diz ao express transformar o JSON em java script object
+app.use(express.json());
+
+/*
+métodos HTTP:
+
+GET: Buscar informação
+POST: Criar informação
+PUT: Alterar informação
+DELETE: Deleta informação
+*/
+
+/*
+Tipos de parâmetros:
+
+Query params: após ? (filtros paginação)
+Route params: indetificar recursos, exemplo: /id
+Request Body: corpo da requisição, ex: criar ou alterar algo
+*/
+
+app.post('/users/', (request, response) =>{
+    
+    const body = request.body;
+
+    console.log(body);
+    
+    return response.json(body);
 });
 
 //configura porta da aplicação
